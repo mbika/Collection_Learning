34474b01230ddbfdf025034b74958088c09877be
Bikash Mohapatra <mbikash.ph@gmail.com>
2022-02-04 23:38:16 +0530

    Collection ArrayList add method Example added sucessfully

---
+++ src/com/technostack/arrayList/ArrayList_Example_1.java
@@ -0,0 +1,17 @@
+package com.technostack.arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList_Example_1 {
+
+	public static void main(String[] args) {
+		List<Integer> aList = new ArrayList<>();
+		aList.add(1);
+		aList.add(2);
+		aList.add(3);
+		aList.add(4);
+		aList.add(5);
+		System.out.println("The arrayList contains data: " + aList);
+	}
+}
