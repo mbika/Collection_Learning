147bd9fe118bdd39efaed475311afe2c49b696f3
Bikash Mohapatra <mbikash.ph@gmail.com>
2022-04-03 20:41:56 +0530

    CSL-5: New method added sucessfully

---
+++ src/com/technostack/arrayList/ArrayList_Example_3.java
@@ -0,0 +1,16 @@
+package com.technostack.arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList_Example_3 {
+
+	public static void main(String[] args) {
+		List<String> al = new ArrayList<>();
+		al.add("apple");
+		al.add("ball");
+		al.add("cat");
+		al.add("dog");
+		System.out.println("Object Present in Arraylist"+al);
+	}
+}
