5ee942fe1c429ab75af510960c03f55b9b64fcae
Bikash Mohapatra <mbikash.ph@gmail.com>
2022-04-03 21:15:13 +0530

    CSL-5:Added a new method as well as docs for easy understanding of code

--- src/com/technostack/arrayList/ArrayList_Example_3.java
+++ src/com/technostack/arrayList/ArrayList_Example_3.java
@@ -5,12 +5,17 @@ import java.util.List;
 
 public class ArrayList_Example_3 {
 
+	/* *	
+	 	This method is written with purpose of how to use ArrayList
+	  	in the Project.
+	 */
 	public static void main(String[] args) {
 		List<String> al = new ArrayList<>();
 		al.add("apple");
 		al.add("ball");
 		al.add("cat");
 		al.add("dog");
+		al.add("elephant");
 		System.out.println("Object Present in Arraylist"+al);
 	}
 }
---
+++ src/com/technostack/arrayList/ArrayList_Example_4.java
@@ -0,0 +1,15 @@
+package com.technostack.arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList_Example_4 {
+
+	public static void main(String[] args) {
+		List<Integer> iList = new ArrayList<>();
+		iList.add(10);
+		iList.add(20);
+		iList.add(50);
+		System.out.println("Object added successfully");
+	}
+}
