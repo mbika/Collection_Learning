9e2a98c655c5e164e66158fa982568107e839a42
Bikash Mohapatra <mbikash.ph@gmail.com>
2022-04-03 20:31:17 +0530

    CSL-5: Added a method sucessfully

---
+++ src/com/technostack/arrayList/ArrayList_Example_2.java
@@ -0,0 +1,15 @@
+package com.technostack.arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList_Example_2 {
+
+	public static void main(String[] args) {
+		List<Integer> iList = new ArrayList();
+		iList.add(10);
+		iList.add(20);
+		System.out.println("Object Present in the ArrayList"+iList);
+		
+	}
+}
